5b7e4264975c8964acd80c9d417f5e494fe69700
Fabio Zunta <nonameeesg@gmail.com>
2022-04-25 17:20:05 -0300

    new organization, random color picker, audio handler and button listeners working well

--- simon-game-starting-files/game.js
+++ simon-game-starting-files/game.js
@@ -1,6 +1,7 @@
-//  
+//  starting declarations
 var allColors = ["green", "red", "yellow", "blue"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 // random color picker
 function nextSequence() {
@@ -9,4 +10,27 @@ function nextSequence() {
     return randomChosenColor;
 }
 
-// gamePattern.push(nextSequence());
+gamePattern.push(nextSequence());
+
+function playSound(color){
+    var audio = new Audio("./sounds/" + color + ".mp3");
+    audio.play();
+}
+
+function buttonPressed(color){
+    $("#" + color).addClass("pressed");
+    setTimeout(function(){
+        $("#" + color).removeClass("pressed");
+    }, 100);
+    playSound();
+}
+
+// buttonPressed(gamePattern[0]);
+
+for (i = 0; i < (allColors.length); i++) {
+    $("#" + allColors[i]).click("click", function(){
+    userClickedPattern.push(this.id);
+    // console.log(userClickedPattern);
+    });
+}
+
